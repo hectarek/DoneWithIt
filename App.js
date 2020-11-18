93c813bd3093e5dbf0f7bb6ee7481c73aac9af25
Hector Gonzalez <hectarek13@gmail.com>
2020-11-18 13:44:16 -0500

    first commit

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log("App is on");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Hector!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,13 @@
+# Done With It App
+
+App that lets you deal with things that you are done with. 
+
+## Notes: 
+
+- View 
+
+View is similar to a div as it represents the view on the screen.
+
+- Expo 
+
+You can pull up the developer menu using cmd + d or ctrl + d
