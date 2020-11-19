752a1728288c28306841ec48fd4eb8d648f3a4fb
Hector Gonzalez <hectarek13@gmail.com>
2020-11-19 13:58:26 -0500

    apparently there is a change I made

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
         left: 30,
     },
     container: {
-        backgroundColor: "#000",
+        backgroundColor: colors.black,
         flex: 1,
     },
     deleteIcon: {
